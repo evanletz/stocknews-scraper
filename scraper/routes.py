aa38dfd525eed34f5c53a46e24a356e4e82abcde
Evan Letzter <evanletzter@gmail.com>
2021-06-14 00:09:35 -0400

    Show all articles in db on homepage

--- scraper/routes.py
+++ scraper/routes.py
@@ -1,20 +1,9 @@
 from flask import render_template
 from scraper import app
+from scraper.models import Article
 
-
-articles = [
-    {'ticker_id': 'CHWY',
-     'title': 'Test Title 1',
-     'url': 'http://testurl1.com/test1'},
-    {'ticker_id': 'AAPL',
-     'title': 'Test Title 2',
-     'url': 'http://testurl2.com/test2'},
-    {'ticker_id': 'NIO',
-     'title': 'Test Title 3',
-     'url': 'http://testurl3.com/test3'}
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', articles=articles)
+    return render_template('home.html', articles=Article.query.all())
