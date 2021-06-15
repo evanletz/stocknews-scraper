32dedfbcf80883670e9e274a6d50afe4cdf10d0e
Evan Letzter <evanletzter@gmail.com>
2021-06-14 21:34:20 -0400

    Add secret key

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd1e86a02a9c8e6e2303326acbe4d5b8d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
