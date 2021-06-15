331093bf13c83e7e42a10bc03d3de2ff0e554311
Evan Letzter <evanletzter@gmail.com>
2021-06-14 23:31:32 -0400

    Add phone field to registration form

--- scraper/forms.py
+++ scraper/forms.py
@@ -6,6 +6,7 @@ class RegistrationForm(FlaskForm):
     
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
