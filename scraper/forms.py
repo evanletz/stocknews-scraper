03cbcd03288cf35e19c94835b21adc633cc125da
Evan Letzter <evanletzter@gmail.com>
2021-06-15 00:03:30 -0400

    Validate username, email, & phone on registration

--- scraper/forms.py
+++ scraper/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+import phonenumbers
+from scraper.models import User
+
 
 class RegistrationForm(FlaskForm):
     
@@ -10,6 +13,21 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username already exists. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email already exists. Please choose a different one.')
+
+    def validate_phone(self, phone):
+        parsed_phone = phonenumbers.parse(phone.data, 'US')
+        if not phonenumbers.is_valid_number(parsed_phone):
+            raise ValidationError('Invalid phone number. Try retyping without symbols.')
 
 class LoginForm(FlaskForm):
     
