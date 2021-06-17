68ba9551d3c8a62acd85fcd590a578883967c867
Evan Letzter <evanletzter@gmail.com>
2021-06-17 00:16:54 -0400

    Change navigation bar if user is logged in or out

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd1e86a02a9c8e6e2303326acbe4d5b8d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
--- scraper/routes.py
+++ scraper/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user
 from scraper import app, db, bcrypt
 from scraper.models import User, Article
 from scraper.forms import RegistrationForm, LoginForm
@@ -12,6 +12,8 @@ def home():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated():
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         pw_hash = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -24,6 +26,8 @@ def register():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -33,3 +37,8 @@ def login():
         else:
             flash('Incorrect email and/or password. Try again.', category='danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
