dc8cebd88157963aec1944a1f1949cfc02e99777
Evan Letzter <evanletzter@gmail.com>
2021-06-17 00:34:27 -0400

    Redirect next_page after login if login required

--- scraper/routes.py
+++ scraper/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from scraper import app, db, bcrypt
 from scraper.models import User, Article
@@ -33,7 +33,8 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Incorrect username and/or password. Try again.', category='danger')
     return render_template('login.html', title='Login', form=form)
