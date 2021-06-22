a9554fe3da2543b925b3b3add0c8096b44d7f4f0
Evan Letzter <evanletzter@gmail.com>
2021-06-22 00:20:12 -0400

    More helpful variable names

--- scraper/utils.py
+++ scraper/utils.py
@@ -33,10 +33,10 @@ def compare_tickers_table(ticker_list):
     # The ticker symbol of this list is the ticker_id
     original_symbols = [t.ticker_id for t in Ticker.query.all()]
 
-    to_delete = list(set(original_symbols) - set(symbol_list))
-    to_add = list(set(symbol_list) - set(original_symbols))
-    to_add = [ticker for ticker in ticker_list if ticker[0] in to_add]
-    return to_delete, to_add
+    symbols_to_delete = list(set(original_symbols) - set(symbol_list))
+    symbols_to_add = list(set(symbol_list) - set(original_symbols))
+    tickers_to_add = [ticker for ticker in ticker_list if ticker[0] in to_add]
+    return symbols_to_delete, tickers_to_add
 
     
 if __name__ == '__main__':
