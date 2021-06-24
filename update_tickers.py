571608d65df113e93f3b80403200e2446778c779
Evan Letzter <evanletzter@gmail.com>
2021-06-24 17:49:29 -0400

    Create recurring script update_tickers.py

---
+++ update_tickers.py
@@ -0,0 +1,20 @@
+import time
+from schedule import repeat, every, run_pending
+from scraper.utils import get_all_tickers, compare_tickers_table, update_tickers_table
+
+
+@repeat(every().day.at('04:00:00'))
+def main():
+    '''
+    Each day at 4 AM local, get an updated list of stock tickers via web scraping.
+    Update the tickers from the Ticker table in the database as needed.
+    '''
+    tickers = get_all_tickers()()
+    delete, add = compare_tickers_table(tickers)
+    update_tickers_table(delete, add)
+
+
+if __name__ == '__main__':
+    while True:
+        run_pending()
+        time.sleep(1)
