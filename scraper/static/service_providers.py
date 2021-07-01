db0ded1c0c2e9e2533c049d1813930577a5083a5
Evan Letzter <evanletzter@gmail.com>
2021-06-30 22:56:03 -0400

    Create SERVICE dict for phone provider addresses

---
+++ scraper/static/service_providers.py
@@ -0,0 +1,8 @@
+SERVICE = {
+    'Verizon': 'vtext.com',
+    'AT&T': 'txt.att.net',
+    'Boost Mobile': 'myboostmobile.com',
+    'Metro PCS': 'mymetropcs.com',
+    'Sprint': 'messaging.sprintpcs.com',
+    'T-Mobile': 'tmomail.net',
+}
