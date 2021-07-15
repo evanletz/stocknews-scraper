6581556eb8c0514aadd7bdfadde63784fbef91ef
Evan Letzter <evanletzter@gmail.com>
2021-07-15 01:39:36 -0400

    Initialize the flask-mail server

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,9 +1,12 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
-
+load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd1e86a02a9c8e6e2303326acbe4d5b8d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -12,5 +15,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # 'login' -> function name of the route
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('FROM')
+app.config['MAIL_PASSWORD'] = os.getenv('PASS')
+mail = Mail(app)
 
 from scraper import routes
