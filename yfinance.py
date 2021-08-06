f18b3ce8e7f22c5a30934198110e049b5ada394b
Evan Letzter <evanletzter@gmail.com>
2021-08-05 21:07:07 -0400

    Remove schedule & add print statements

--- yfinance.py
+++ yfinance.py
@@ -4,25 +4,28 @@ from scraper.yfinance.utils import get_user_tickers, diff_headline, to_article_o
 from scraper.yfinance.models import Yfinance, Bit, EmailClient
 
 
-@repeat(every(10).minutes)
-def main():
-    tickers = get_user_tickers()
-    print(tickers)
-    yf = Yfinance()
-    for ticker in tickers:
-        response = yf.request_page(ticker)
-        article_info = yf.parse_html(response, ticker)
-        if diff_headline(article_info['url'], ticker):
-            article_info['url_shortened'] = Bit().shorten(article_info['url'])
-            article_obj = to_article_obj(article_info)
-            commit_article(article_obj)
-            contacts = get_all_contacts(ticker)
-            mail = EmailClient()
-            for contact in contacts:
-                mail.send_text(contact, article_obj)
+def yf(app):
+    with app.app_context():
+        print('Starting YF')
+        tickers = get_user_tickers()
+        print(f'\tTickers: {tickers}')
+        yf = Yfinance()
+        for ticker in tickers:
+            response = yf.request_page(ticker)
+            article_info = yf.parse_html(response, ticker)
+            if diff_headline(article_info['url'], ticker):
+                print(f'{article_info['ticker']}: {article_info['title']}')
+                article_info['url_shortened'] = Bit().shorten(article_info['url'])
+                article_obj = to_article_obj(article_info)
+                commit_article(article_obj)
+                contacts = get_all_contacts(ticker)
+                print(f'Sending messages to {len(contacts)} users')
+                mail = EmailClient()
+                for contact in contacts:
+                    mail.send_text(contact, article_obj)
+                print('Sent!')
+        print('Finished YF')
 
 
 if __name__ == '__main__':
-    while True:
-        run_pending()
-        time.sleep(1)
+    pass
