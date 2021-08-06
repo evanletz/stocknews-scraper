c611b501e6804ac0a13255af091098d590485bb3
Evan Letzter <evanletzter@gmail.com>
2021-08-05 21:11:17 -0400

    Schedule YT & UT funcs & add to app context

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 from scraper import create_app
+from update_tickers import ut
+from yfinance import yf
 
 
 app = create_app()
 
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(lambda: ut(app), 'cron', hour=20, minute=17)
+sched.add_job(lambda: yf(app), 'interval', seconds=10)
+sched.start()
+
+@atexit.register
+def close():
+    print('Shutting down background tasks ...')
+    sched.shutdown(wait=False)
+    print('Success!')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
